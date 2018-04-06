6a2c9a39fa5eca94d6397bd6db876e6977260236
karllaurence <liunan624124755@gmail.com>
2018-04-06 23:42:49 +0800

    minor change

--- app/Command/DailyMail.php
+++ app/Command/DailyMail.php
@@ -52,7 +52,7 @@ class DailyMail
         
         $sts = new Analytics();
         
-        Telegram::Send($config["appName"]." 的主人，系统今天的运行状况如下:".
+        Telegram::Send("主人，今天系统的运行状况如下:".
         PHP_EOL.
         "今日签到人数:".$sts->getTodayCheckinUser().PHP_EOL.
         "今日使用总流量:".Tools::flowAutoShow($lastday_total).PHP_EOL.
