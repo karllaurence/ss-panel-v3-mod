48658438765f695f06791913668ab2cdc8cf9b1e
karllaurence <liunan624124755@gmail.com>
2018-04-15 22:22:07 +0800

    minor change

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -23,7 +23,7 @@ class Shop extends Model
                     $content_text .= "为账号的有效期添加 ".$value." 天，";
                     break;
                 case "class":
-                    $content_text .= "为账号升级为等级 ".$value." ，有效期 ".$content["class_expire"]." 天，";
+                    $content_text .= "为账号升级为等级 ".$value."，有效期 ".$content["class_expire"]." 天，";
                     break;
                 case "reset":
                     $content_text .= "在 ".$content["reset_exp"]." 天内，每 ".$value." 天重置流量为 ".$content["reset_value"]." G ";
